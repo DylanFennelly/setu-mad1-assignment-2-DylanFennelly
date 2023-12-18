7bc810b6e303aac0a9763e4a18203e1ebd818abf
DylanFennelly <dylanantfennelly@gmail.com>
2023-12-18 21:32:19 +0000

    Created CharacterNavGraph, added navigation dependency

---
+++ app/src/main/java/com/example/characterapp/ui/navigation/CharacterNavGraph.kt
@@ -0,0 +1,13 @@
+package com.example.characterapp.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun CharacterNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+
+}
