1227a13abcd357e6bb29fb888b39f7fc2b4745a6
DylanFennelly <dylanantfennelly@gmail.com>
2023-12-21 14:24:11 +0000

    Created CharacterDatabase & Updated Kotlin + Compose versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.6"        //https://developer.android.com/jetpack/androidx/releases/compose-kotlin
     }
     packaging {
         resources {
