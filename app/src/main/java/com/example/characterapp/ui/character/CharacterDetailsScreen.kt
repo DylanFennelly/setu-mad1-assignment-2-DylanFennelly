130d152280588092b75ac1127e4091f59466bd76
DylanFennelly <dylanantfennelly@gmail.com>
2023-12-21 21:23:06 +0000

    Create outline of characterDetailsScreen + preview

--- app/src/main/java/com/example/characterapp/ui/character/CharacterDetailsScreen.kt
+++ app/src/main/java/com/example/characterapp/ui/character/CharacterDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.characterapp.ui.character
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,11 +11,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.characterapp.CharacterTopAppBar
 import com.example.characterapp.R
+import com.example.characterapp.data.CharacterModel
 import com.example.characterapp.ui.AppViewModelProvider
 import com.example.characterapp.ui.navigation.NavigationDestination
+import com.example.characterapp.ui.theme.CharacterAppTheme
 
 object CharacterDetailsDestination : NavigationDestination {
     override val route = "character_details"
@@ -44,8 +48,57 @@ fun CharacterDetailsScreen(
                navigateUp = onNavigateUp
            )
        }
-   ) {
-
+   ) {innerPadding ->
+        CharacterDetailsBody(
+            characterDetailsUiState = uiState.value
+        )
    }
+}
 
+@Composable
+private fun CharacterDetailsBody(
+    characterDetailsUiState: CharacterDetailsUiState,
+    modifier: Modifier = Modifier
+){
+    Column(
+
+    ) {
+        CharacterDetailsInfo(
+            characterModel = characterDetailsUiState.characterDetails.toCharacter()
+        )
+    }
+
+}
+
+@Composable
+fun CharacterDetailsInfo(
+    characterModel: CharacterModel, modifier: Modifier = Modifier
+){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemDetailsBodyPreview() {
+    CharacterAppTheme {
+        CharacterDetailsBody(
+            characterDetailsUiState = CharacterDetailsUiState(
+                characterDetails = CharacterDetails(
+                    name = "Cyn",
+                    race = "Half-Elf",
+                    battleClass = "Ranger",
+                    level = "3",
+                    str = "9",
+                    dex = "16",
+                    con = "12",
+                    int = "8",
+                    wis = "14",
+                    cha = "11",
+                    maxHP = "25",
+                    ac = "13",
+                    background = "Criminal"
+                )
+            )
+        )
+    }
 }
